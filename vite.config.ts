686fa424b37fe5c2976089506ba7bb41e8278073
Jerome Hunter <hunt0497@algonquinlive.com>
2026-01-31 16:29:15 -0500

    Fix base path for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/jerome-hunter-portfolio/",
+  base: "./",
   plugins: [react()],
   assetsInclude: ["**/*.jpg", "**/*.png", "**/*.svg", "**/*.jpeg", "**/*.webp"],
 });
