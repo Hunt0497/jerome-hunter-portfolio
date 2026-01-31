1df01e16baa8acd6ae33f53b4b344c19068a5117
Jerome Hunter <hunt0497@algonquinlive.com>
2026-01-31 13:36:37 -0500

    Set Vite base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/jerome-hunter-portfolio/",
   plugins: [react()],
-  assetsInclude: ['**/*.jpg', '**/*.png', '**/*.svg', '**/*.jpeg', '**/*.webp']
-})
+  assetsInclude: ["**/*.jpg", "**/*.png", "**/*.svg", "**/*.jpeg", "**/*.webp"],
+});
